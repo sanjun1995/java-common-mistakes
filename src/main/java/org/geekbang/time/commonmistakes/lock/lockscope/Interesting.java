b5546deac9f2d65887ea51940f2a973e33c80590
caozhixin <caozhixin@zhuanzhuan.com>
2022-07-11 19:43:00 +0800

    fix

--- src/main/java/org/geekbang/time/commonmistakes/lock/lockscope/Interesting.java
+++ src/main/java/org/geekbang/time/commonmistakes/lock/lockscope/Interesting.java
@@ -3,6 +3,9 @@ package org.geekbang.time.commonmistakes.lock.lockscope;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Assert;
 
+import java.util.concurrent.Executors;
+import java.util.stream.IntStream;
+
 @Slf4j
 public class Interesting {
 
@@ -10,13 +13,13 @@ public class Interesting {
     static volatile int b = 1;
 
     public static void main(String[] args) {
-        add();
-        compare();
+        Executors.newFixedThreadPool(1).submit(() -> add());
+        Executors.newFixedThreadPool(1).submit(() -> compareRight());
     }
 
     public static synchronized void add() {
         log.info("add start");
-        for (int i = 0; i < 1000000; i++) {
+        for (int i = 0; i < 100000; i++) {
             a++;
             b++;
         }
@@ -25,7 +28,7 @@ public class Interesting {
 
     public static void compare() {
         log.info("compare start");
-        for (int i = 0; i < 1000000; i++) {
+        for (int i = 0; i < 100000; i++) {
             if (a < b) {
                 log.info("a:{},b:{},{}", a, b, a > b);
                 //最后的a>b应该始终是false的吗？
